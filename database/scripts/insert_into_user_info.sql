9eb03f707d0d68ebda13cb7fcd786256cdbe5369
JuliaKush01 <78054883+JuliaKush01@users.noreply.github.com>
2023-04-23 18:56:31 -0400

    updates to files

--- database/scripts/insert_into_user_info.sql
+++ database/scripts/insert_into_user_info.sql
@@ -1,12 +1,12 @@
-INSERT INTO `user_info` ( `user`,
+INSERT INTO `user_info` (`user`,
   `password`,
   `wallet_balance`,
   `share_holdings`,
   `buy_price`)
 
 VALUES
-("Julia Kush", "gorams", "1000", "Apple", "250")
-("Brandon Kohler", "gorams", "2300", "Microsoft", "200")
-("Brian Giovinazzo", "gorams1", "1500", "Johnson and Johnson", "180")
-("Jacob Peterson", "gorams12", "3000", "Tesla", "170")
-("Stephen Caliendo", "gorams123", "1800", "Google", "180");
+("Julia Kush", "gorams", "1000", "Apple", 250),
+("Brandon Kohler", "gorams", "2300", "Microsoft", 200),
+("Brian Giovinazzo", "gorams1", "1500", "Johnson and Johnson", 180),
+("Jacob Peterson", "gorams12", "3000", "Tesla", 170),
+("Stephen Caliendo", "gorams123", "1800", "Google", 180);
