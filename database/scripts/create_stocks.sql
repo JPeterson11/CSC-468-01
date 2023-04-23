027d5ac1bb7cacad9f000acb958cab281c23c0f1
JuliaKush01 <78054883+JuliaKush01@users.noreply.github.com>
2023-04-23 18:27:35 -0400

    updates to files

--- database/scripts/create_stocks.sql
+++ database/scripts/create_stocks.sql
@@ -1,15 +1,15 @@
-CREATE DATABASE IF NOT EXISTS 'STOCKS';
+CREATE DATABASE IF NOT EXISTS `STOCKS`;
 GO
-USE 'STOCKS';
+USE `STOCKS`;
 GO
 CREATE TABLE 'stock_info' (
-  'stock_name' varchar(100) NOT NULL,
-  'recent_price' varchar(50) NOT NULL,
-  'purchase_price' varchar(50) NOT NULL,
-  '10_day_price' varchar(50) NOT NULL,
-  'monthly_price' varchar(50) NOT NULL,
-  'quantity' varchar(50) NOT NULL,
-  'purchase_date' varchar(50) NOT NULL,
-  'sell_date' varchar(50) NOT NULL,
-  'todays_date' varchar(50) NOT NULL
+  `stock_name` varchar(100) NOT NULL,
+  `recent_price` varchar(50) NOT NULL,
+  `purchase_price` varchar(50) NOT NULL,
+  `10_day_price` varchar(50) NOT NULL,
+  `monthly_price` varchar(50) NOT NULL,
+  `quantity` varchar(50) NOT NULL,
+  `purchase_date` varchar(50) NOT NULL,
+  `sell_date` varchar(50) NOT NULL,
+  `todays_date` varchar(50) NOT NULL
 );
--- database/scripts/create_user_info.sql
+++ database/scripts/create_user_info.sql
@@ -1,11 +1,11 @@
-CREATE DATABASE IF NOT EXISTS 'STOCKS';
+CREATE DATABASE IF NOT EXISTS `STOCKS`;
 GO
-USE 'STOCKS';
+USE `STOCKS`;
 GO
 CREATE TABLE 'user_info' (
-  'user' varchar(100) NOT NULL,
-  'password' varchar(50) NOT NULL,
-  'wallet_balance' varchar(50) NOT NULL,
-  'share_holdings' varchar(50) NOT NULL,
-  'buy_price' varchar(50) NOT NULL
+  `user` varchar(100) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `wallet_balance` varchar(50) NOT NULL,
+  `share_holdings` varchar(50) NOT NULL,
+  `buy_price` varchar(50) NOT NULL
 );
