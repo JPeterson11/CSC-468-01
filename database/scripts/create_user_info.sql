1aab200b81b56fc7afcd320565b4d0e6e14ededa
JuliaKush01 <78054883+JuliaKush01@users.noreply.github.com>
2023-04-23 17:30:42 -0400

    updates to database files

---
+++ database/scripts/create_user_info.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS 'STOCKS';
+USE 'STOCKS';
+GO
+CREATE TABLE 'user_info' (
+  'user' varchar(100) NOT NULL,
+  'password' varchar(50) NOT NULL,
+  'wallet_balance' varchar(50) NOT NULL,
+  'share_holdings' varchar(50) NOT NULL,
+  'buy_price' varchar(50) NOT NULL
+)
