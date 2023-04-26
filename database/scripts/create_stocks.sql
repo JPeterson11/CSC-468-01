d1f40f5f488f25df6f90bd65731c6ba88e9f47c4
JuliaKush01 <78054883+JuliaKush01@users.noreply.github.com>
2023-04-26 18:33:51 -0400

    updates

--- database/scripts/create_stocks.sql
+++ database/scripts/create_stocks.sql
@@ -1,7 +1,15 @@
-CREATE DATABASE IF NOT EXISTS `STOCKS`;
 GO
 USE `STOCKS`;
 GO
-ALTER TABLE `stock_info` 
-MODIFY COLUMN `todays_date` DATE;
+CREATE TABLE `stock_info` (
+  `stock_name` varchar(100) NOT NULL,
+  `recent_price` varchar(50) NOT NULL,
+  `purchase_price` varchar(50) NOT NULL,
+  `10_day_price` varchar(50) NOT NULL,
+  `monthly_price` varchar(50) NOT NULL,
+  `quantity` varchar(50) NOT NULL,
+  `purchase_date` varchar(50) NOT NULL,
+  `sell_date` varchar(50) NOT NULL,
+  `todays_date` date
+);
 GO
--- database/scripts/insert_into_stocks.sql
+++ database/scripts/insert_into_stocks.sql
@@ -1,9 +1,16 @@
-INSERT INTO `stock_info` (
+INSERT INTO `stock_info` (`stock_name`,
+  `recent_price`,
+  `purchase_price`,
+  `10_day_price`,
+  `monthly_price`,
+  `quantity`,
+  `purchase_date`,
+  `sell_date`,
   `todays_date`)
 
 VALUES
- (curdate()),
- (curdate()),
- (curdate()),
- (curdate()),
- (curdate());
+  ("Tesla", "199", "195.86", "200", "205", "1", "2/24/2023", "3/29/2023", curdate()),
+  ("Microsoft", "230", "283.34", "233", "244", "1", "1/14/2023", "3/19/2023", curdate()),
+  ("Johnson & Johnson", "163.99", "153.44", "145", "187", "1", "1/09/2023", "2/28/2023", curdate()),
+  ("Apple", "140", "162.07", "150", "155", "3", "11/14/2022", "3/10/2023", curdate()),
+  ("American Express", "133.34", "162.43", "162.90", "171.97", "2", "1/12/2022", "1/03/2023", curdate());
