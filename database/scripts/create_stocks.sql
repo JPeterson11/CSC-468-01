499ea237bbfe8b301252eb3a0a28ac9827fa6910
JuliaKush01 <78054883+JuliaKush01@users.noreply.github.com>
2023-04-26 18:05:11 -0400

    updates

--- database/scripts/create_stocks.sql
+++ database/scripts/create_stocks.sql
@@ -2,15 +2,15 @@ CREATE DATABASE IF NOT EXISTS `STOCKS`;
 GO
 USE `STOCKS`;
 GO
-CREATE TABLE `stock_info` (
-  `stock_name` varchar(100) NOT NULL,
-  `recent_price` varchar(50) NOT NULL,
-  `purchase_price` varchar(50) NOT NULL,
-  `10_day_price` varchar(50) NOT NULL,
-  `monthly_price` varchar(50) NOT NULL,
-  `quantity` varchar(50) NOT NULL,
-  `purchase_date` varchar(50) NOT NULL,
-  `sell_date` varchar(50) NOT NULL,
-  `todays_date` varchar(50) NOT NULL
+ALTER TABLE `stock_info` (
+  -- `stock_name` varchar(100) NOT NULL,
+  -- `recent_price` varchar(50) NOT NULL,
+  -- `purchase_price` varchar(50) NOT NULL,
+  -- `10_day_price` varchar(50) NOT NULL,
+  -- `monthly_price` varchar(50) NOT NULL,
+  -- `quantity` varchar(50) NOT NULL,
+  -- `purchase_date` varchar(50) NOT NULL,
+  -- `sell_date` varchar(50) NOT NULL,
+  `todays_date` date
 );
 GO
